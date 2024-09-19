4325d5a03cf945a4b74d4ce5fce230c441a3bc0d
談議必統 <2220213@ecc.ac.jp>
2024-09-19 17:53:05 +0900

    disSpeed追加

--- Assets/Script/GameSystem.cs
+++ Assets/Script/GameSystem.cs
@@ -9,7 +9,7 @@ public class GameSystem : MonoBehaviour
     public float BaseSpeed=5.0f;
     public float MaxBossDistance = 1000.0f;
     public float GamePosition = 0.0f;
-    
+    public float disSpeed=5.0f;//被弾でスピード下がる
     public Boss boss; // ボスの参照
     public bool isBossActive = false; // ボスがアクティブかどうか
     //public float 
@@ -36,7 +36,10 @@ public class GameSystem : MonoBehaviour
     {
         BaseSpeed += speed;
     }
-
+    public void DisSpeed()
+    {
+        BaseSpeed -= disSpeed;
+    }
 
     // Update is called once per frame
     void Update()
