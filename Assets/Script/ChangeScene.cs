e25c238b4201779a00b0a4e1a1a442c799e06ac5
KingClub7 <96010932+KingClub7@users.noreply.github.com>
2024-09-20 16:06:03 +0900

    Revert "Revert "Revert "Merge branch 'mori'"""
    This reverts commit f2640764ff8dcb4951e86e8e06e075a00aaaf0f8.

--- Assets/Script/ChangeScene.cs
+++ Assets/Script/ChangeScene.cs
@@ -6,18 +6,10 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public Image spaceTitle;
-    public Image spaceResult;
-
-    bool loadOnceTitle = false;
-    bool loadOnceGame = false;
-    bool loadOnceResult = false;
-
     // Start is called before the first frame update
     void Start()
     {
-        loadOnceTitle = false;
-        loadOnceResult = false;
+        
     }
 
     // Update is called once per frame
@@ -26,51 +18,36 @@ public class ChangeScene : MonoBehaviour
         switch(SceneManager.GetActiveScene().name)
         {
             case "TitleScene":
-                ButtonPush bpTitle = spaceTitle.GetComponent<ButtonPush>();
                 if (Input.GetKeyDown("space"))
                 {
-                    bpTitle.enabled = true;
-                }
-                if (bpTitle.GetIsFinish()&& !loadOnceTitle)
-                {
-                    loadOnceTitle = true;
-                    FadeManager.Instance.LoadScene("GameScene", 0.3f);
 
-                    //SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+                    SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
                     GameSystem.Instance.isClear = false;
                     GameSystem.Instance.isBossActive = false;
                     GameSystem.Instance.GamePosition = 0;
                     GameSystem.Instance.BaseSpeed = 5;
                 }
-                    break;
+                break;
 
             case "GameScene":
-                if (PlayerScript.instance.Energy < 0 && !loadOnceGame)
+                if (PlayerScript.instance.Energy < 0) 
                 {
-                    FadeManager.Instance.LoadScene("ResultScene", 0.3f);
-                    loadOnceGame = true;
+                    //GameSystem.Instance.isClear = false;
+                    SceneManager.LoadScene("ResultScene", LoadSceneMode.Single);
                 }
                 if(GameSystem.Instance.isBossActive)
                 {
-                    if (GameSystem.Instance.isClear && !loadOnceGame)
+                    if (GameSystem.Instance.isClear)
                     {
-                        FadeManager.Instance.LoadScene("ResultScene", 0.3f);
-                        loadOnceGame = true;
+                        SceneManager.LoadScene("ResultScene", LoadSceneMode.Single);
                     }
                 }
                     break;
-               
 
             case "ResultScene":
-                ButtonPush bpResult = spaceResult.GetComponent<ButtonPush>();
                 if (Input.GetKeyDown("space"))
                 {
-                    bpResult.enabled = true;
-                }
-                if (bpResult.GetIsFinish() && !loadOnceResult)
-                {
-                    loadOnceResult = true;
-                    FadeManager.Instance.LoadScene("TitleScene", 0.3f);
+                    SceneManager.LoadScene("TitleScene", LoadSceneMode.Single);
                 }
                 break;
         }
